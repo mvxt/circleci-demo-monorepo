e264a300e3fa48d2ca7772dc1a48c0edbc409460
Michael V Thanh <mthanh@circleci.com>
2020-03-09 17:29:06 -0600

    Add go modules, add .gitignore

--- user/main.go
+++ user/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+  "flag"
   "fmt"
+  "github.com/golang/glog"
 )
 
 func Hello() string {
+  glog.Info("Calling Hello()")
   return "Hello User!"
 }
 
 func main() {
+  flag.Parse()
 	fmt.Println(Hello())
 }
